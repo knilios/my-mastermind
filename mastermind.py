a20468f09be7b7b2327016babf5dabd0be10535f
Kristawan Jaroenrad <kristawanja@gmail.com>
2023-11-16 09:31:10 +0700

    make oop version works

--- mastermind.py
+++ mastermind.py
@@ -1,34 +1,55 @@
 import random, sys
 
-print("Welcome to Nattochan the annoying chatbot's parent mastermind game. Prepare for imminent defeat! Good luck, I guess.")
-numbers = tuple(range(int(input("Enter an amount of numbers: "))))
-answer_amount = int(input("Enter answer amount: "))
-number_tries = int(input("Enter number of tries: "))
-if answer_amount > len(numbers):
-    print("Hmm, looks like you need a math lesson! Please enter the correct amount of numbers to guess, unless you enjoy this entertaining display of incompetence.")
-print("Your available numbers are :", numbers)
-print("Entering format: [your first number choice]space[your second number choice]space...space[your last number choice]")
-print("For example: 1 2 3 4")
-correct_answer = random.sample(numbers, answer_amount)
-for times in range(number_tries):
-    while True:
-        user_guess = input("Enter your guess: ")
-        _list = user_guess.split(" ")
-        if len(_list) != answer_amount:
-            print(f"Did you forget how to type? Please enter a valid input this time, unless you enjoy embarrassing yourself. (you can only enter {answer_amount} slots)")
-            continue
-        break
-    correct_position = sum([1 for i in range(len(_list)) if (int(_list[i]) == int(correct_answer[i]))])
-    correct_existence = sum ([1 for i in _list if int(i) in correct_answer]) - correct_position
-    if correct_position == answer_amount:
-        print("Congratulations, you actually did it! You won! But don't get too confident, it was probably just luck.")
+class Mastermind:
+    def __init__(self):
+        print("Welcome to Emotional Damage Mastermind Game. Prepare for imminent defeat! Good luck, I guess.")
+        self.__numbers = range(1, 9) #tuple(range(int(input("Enter an amount of numbers: "))))
+        self.__answer_amount = 4#int(input("Enter answer amount: "))
+        # self.__number_tries = int(input("Enter number of tries: "))
+        if self.__answer_amount > len(self.__numbers):
+            print("Hmm, looks like you need a math lesson! Please enter the correct amount of numbers to guess, unless you enjoy this entertaining display of incompetence.")
+            sys.exit()
+        self.__correct_answer = random.sample(self.__numbers, self.__answer_amount)
+
+
+    def __check(self, _list):
+        correct_position = sum([1 for i in range(len(_list)) if (int(_list[i]) == int(self.__correct_answer[i]))])
+        correct_existence = sum ([1 for i in list(set(_list)) if int(i) in self.__correct_answer]) - correct_position
+        return correct_position, correct_existence
+
+    def __get_input(self):
+        while True:
+            user_guess = input("What is your guess?: ")
+            _list = [i for i in user_guess]
+            if len(_list) != self.__answer_amount:
+                print(f"Did you forget how to type? Please enter a valid input this time, unless you enjoy embarrassing yourself. (you can only enter {self.__answer_amount} slots)")
+                continue
+            return _list
+        
+    def play(self):
+        
+        count = 1
+        while True:
+            _list = self.__get_input()
+            correct_position, correct_existence = self.__check(_list)
+            print("Your guess is: ", end="")
+            [print(i, end="") for i in _list]
+            print()
+            print("*"*correct_position, end="")
+            print("o"*correct_existence, end="")
+            print()
+            if correct_position == self.__answer_amount:
+                print(f"Congratulations, you actually did it after {count} attempts! You won! But don't get too confident, it was probably just luck.")
+                sys.exit()
+            count += 1
+
+    def __get_correct_answer(self):
+        print(self.__correct_answer)
+    
+    def end_game(self):
+        print("Well, looks like someone couldn't handle the heat! Don't worry, I'll still be here, having fun without you. Better luck next time!")
         sys.exit()
-    print("correct positions: ", correct_position)
-    print("correct existences: ", correct_existence)
-    if times == number_tries -1 : 
-        break
-    print(f"Nice try, but that's not the correct answer. You still have { number_tries -1 - times} attempts left. Keep going!")
+        
 
-
-print("Oops! Looks like you ran out of tries. Better luck next time, loser! Maybe try a simpler game next time, hmm?")
-print("(correct answer : ", correct_answer, ")")
+my_mastermind = Mastermind()
+my_mastermind.play()
